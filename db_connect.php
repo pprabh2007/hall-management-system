332bbc5fb711c6ff49358e717a63e425cfa97faf
pprabh2007 <pprabh2007@hotmail.co.uk>
2020-03-18 23:17:42 +0530

    add connection

--- db_connect.php
+++ db_connect.php
@@ -1,21 +1,22 @@
 <?php
-  if(!defined('CONN_C'))
-  	die('<h1>Direct Access Denied.</h1>');
-  $mysql_host = 'localhost';
-  $mysql_user = 'root';
-  $mysql_pword = '';
-  $mysql_db = 'hall_management_db';
-  $conn = null;
-  $query = '';
-  $query_run = null;
-  $data = null;
-  try
-  {
-  	$conn = new PDO('mysql:host='.$mysql_host.';dbname='.$mysql_db, $mysql_user,$mysql_pword);
-  	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  }
-  catch(PDOException $e)
-  {
-  	die('<h1>Connection failed!</h1>');
-  }
+
+$server_name = 'localhost';
+$server_user_name = 'root';
+$password = '';
+$database_name = 'hms';
+
+try
+{
+	$connection = mysqli_connect($server_name, $server_user_name, $password, $database_name);
+
+	if($connection)
+	{
+		echo "database connected!";
+	}
+}
+catch(Exceptiom $e)
+{
+	echo $e->getMessage();
+}
+
 ?>
